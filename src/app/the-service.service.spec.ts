5a653659689fccd8c8d9a7f9f928dc9b3948e62a
Paweł Żurowski <pawel.zurowski@trinite.io>
2019-10-20 18:05:08 +0200

    test refactor

--- src/app/the-service.service.spec.ts
+++ src/app/the-service.service.spec.ts
@@ -3,10 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import { TheServiceService } from './the-service.service';
 
 describe('TheServiceService', () => {
+  let service: TheServiceService;
+
   beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => service = TestBed.get(TheServiceService));
 
   it('should be created', () => {
-    const service: TheServiceService = TestBed.get(TheServiceService);
     expect(service).toBeTruthy();
   });
 });
