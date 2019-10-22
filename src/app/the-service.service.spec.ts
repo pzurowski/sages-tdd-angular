2397a5940016988a77cb5752419e85d321609d4f
Paweł Żurowski <pawel.zurowski@trinite.io>
2019-10-22 08:56:48 +0200

    http testing

--- src/app/the-service.service.spec.ts
+++ src/app/the-service.service.spec.ts
@@ -1,14 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TheServiceService } from './the-service.service';
 
 describe('TheServiceService', () => {
   let service: TheServiceService;
 
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [TheServiceService],
+  }));
   beforeEach(() => service = TestBed.get(TheServiceService));
+  beforeEach(() => httpMock = TestBed.get(HttpTestingController));
+
+  let httpMock: HttpTestingController;
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should perform sign in', () => {
+    const result = [];
+
+    service.signIn('login', 'password').subscribe(item => result.push(item));
+
+    const req = httpMock.expectOne(`/api/login`);
+    req.flush({});
+    expect(req.request.headers.get('Authorization')).toEqual(
+      'Basic bG9naW46cGFzc3dvcmQ=',
+    );
+    expect(req.request.withCredentials).toEqual(true);
+    expect(req.request.method).toBe('GET');
+    expect(result).toEqual([{}]);
+  });
+
 });
--- src/app/the-service.service.ts
+++ src/app/the-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TheServiceService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public signIn(login, password) {
+    const token = `Basic ${ btoa(`${ login }:${ password }`) }`;
+    const headers = new HttpHeaders({ Authorization: token });
+    return this.httpClient.get(`/api/login`, {
+      headers,
+      withCredentials: true,
+    });
+  }
 }
